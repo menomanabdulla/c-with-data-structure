2fddd03702c93544f187a904e34b7e7716e6d661
nomanabdulla <noman.fcibappy@gmail.com>
2018-11-12 04:03:23 +0600

    Null pointer

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main()
     upper_case = lower_case - 32;
     printf("The capital letter is: %c \n",upper_case);
     */
-
+    //Pointer mainly use for memory management
     int x = 10,y;
     int *p;
     printf("Value of x: %d \n",x);
@@ -28,19 +28,57 @@ int main()
     printf("Address of x: %p \n",&x);
     printf("Address of y: %p \n",&y);
     printf("Value of p: %p \n",p);
+
     printf("\n\n\n");
+    printf("---- Pointer dereferencing ---- \n");
 
     int xx = 10,yy;
     int *pp,*qq;
     pp = &xx;
     qq = &yy;
     yy = *pp;
-    *pp = 15;
-    *qq = 20;
+    *pp = 15; //Pointer dereferencing
+    *qq = 20; //Pointer dereferencing
 
     printf("Value of xx: %d \n",xx);
     printf("Value of yy: %d \n",yy);
     printf("Value of *pp %d \n",*pp);
     printf("Value of *qq %d \n",*qq);
+
+    printf("\n\n\n");
+    printf("---- Null Pointer ---- \n");
+
+    x = 100;
+    int *ppp = NULL;
+    printf("Value of x: %d \n",x);
+    ppp = &x;
+    printf("Value of *ppp: %d \n",*ppp);
+
+    printf("\n\n\n");
+    printf("---- Null Pointer dereferencing test ---- \n");
+
+    int *pppp = NULL;
+    pppp = &x;
+    *pppp = 110;
+    printf("Value of *pppp: %d \n",*pppp);
+
+    printf("\n\n\n");
+    printf("---- String and Pointer ---- \n");
+    char s[] = "Bangladesh";
+    printf("Name of our country : %s\n",s);
+    printf("Address of s: %p \n",s);
+
+    char *p1;
+    p1 = s;
+    printf("Name of our country: %s\n",p1);
+
+    char c1 = 'A', c2 = 'B', c3 = 'C';
+    char *p2,*p3,*p4;
+    p2 = &c1;
+    p3 = &c2;
+    p4 = &c3;
+
+    printf("%c, %c, %c \n",*p2,*p3,*p4);
+
     return 0;
 }
