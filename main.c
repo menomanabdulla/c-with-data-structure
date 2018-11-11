138e8868f1eb9b7aa840f6135fb62f5e46093b90
nomanabdulla <noman.fcibappy@gmail.com>
2018-11-12 03:27:05 +0600

    proper pointer decomposition

--- main.c
+++ main.c
@@ -14,17 +14,33 @@ int main()
     printf("The capital letter is: %c \n",upper_case);
     */
 
-    int x = 10;
+    int x = 10,y;
     int *p;
     printf("Value of x: %d \n",x);
     p = &x;
-    *p = 20;
+    y = *p;
+    *p = 15;
+
     printf("Value of x: %d \n",x);
-    x = 15;
-    printf("Value of x: %d \n",x);
-    printf("Value stored at location %p is %d \n",p,*p);
+    printf("Value of y: %d \n",y);
+    printf("Value of *p %d \n",*p);
 
     printf("Address of x: %p \n",&x);
+    printf("Address of y: %p \n",&y);
     printf("Value of p: %p \n",p);
+    printf("\n\n\n");
+
+    int xx = 10,yy;
+    int *pp,*qq;
+    pp = &xx;
+    qq = &yy;
+    yy = *pp;
+    *pp = 15;
+    *qq = 20;
+
+    printf("Value of xx: %d \n",xx);
+    printf("Value of yy: %d \n",yy);
+    printf("Value of *pp %d \n",*pp);
+    printf("Value of *qq %d \n",*qq);
     return 0;
 }
